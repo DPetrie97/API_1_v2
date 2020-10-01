3711c9c5bc7a6eded5968a148a22e57e43c6e5e0
David Petrie <davidpapasmurf@Davids-MacBook-Pro.local>
2020-10-01 10:27:50 -0400

    update push

--- api.rework.js
+++ api.rework.js
@@ -1,7 +1,7 @@
 console.log('My files are connected');
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/"; // proxyurl to get past the "cors-origin" policy
-const baseURL = "https://cat-fact.herokuapp.com/facts/type/";
+// const proxyurl = "https://cors-anywhere.herokuapp.com/"; // proxyurl to get past the "cors-origin" policy
+const baseURL = "https://cat-fact.herokuapp.com/facts/random?animal_type";
 let url;
 
 // Search Form
@@ -19,7 +19,7 @@ function fetchResultsFacts(e) {
 
     console.log("URL:", url);
 
-    fetch(proxyurl + url) // using URL variable along with the proxyurl to bypass the "cors-original" policy
+    fetch(url) // using URL variable along with the proxyurl to bypass the "cors-original" policy
     .then(function(result) {
         return result.json();
     })
@@ -42,27 +42,26 @@ function fetchResultsFacts(e) {
             resultForm.setAttribute("class", "resultForm");
             let headerTag = document.createElement("h2");
             headerTag.setAttribute("id", "headerTag");
-            let randoClearFix = document.createElement("div");
+            let clearFixRandom = document.createElement("div");
+            clearFixRandom.setAttribute("class", "clearFixRandom");
             let paraFact = document.createElement("para");
             paraFact.setAttribute("class", "catFacts");
             // the tags that appear in the "resultsSection" in the HTML ^
             headerTag.textContent = generatedFacts;
             console.log("Facts:", generatedFacts);
-        }
-
-        paraFact.textContent = "Random Facts: ";
+        
 
         for (let i = 0; i < 1; i++) {
             let spanResult = document.createElement("span");
             spanResult.setAttribute("class", "randoFacts");
-            spanResult.textContent += generatedFacts[0]
+            spanResult.textContent += generatedFacts.text;
             paraFact.appendChild(spanResult);
         }
-        randoClearFix.setAttribute("class", "randoClearFix");
 
         resultForm.appendChild(headerTag);
-        resultForm.appendChild(randoClearFix);
+        resultForm.appendChild(clearFixRandom);
         resultForm.appendChild(paraFact);
         resultsSection.appendChild(resultForm);
     }
+}
 }
